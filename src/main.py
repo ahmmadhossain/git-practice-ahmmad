a7a6c2a81bff8a41bb227ed3b4f1756ae28f2625
Ahmmad Hossain <ahmmad@gmail.com>
2026-06-14 08:06:12 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 from datetime import date
+from utils import add, subtract, multiply
 
 print("Name: Ahmmad Hossain")
 print("Today's Date:", date.today())
+
+print("Add:", add(10, 5))
+print("Subtract:", subtract(10, 5))
+print("Multiply:", multiply(10, 5))
