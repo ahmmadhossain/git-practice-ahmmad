2c5d7a0589afe1a149c5faabc3d524a9d0d2586b
Ahmmad Hossain <ahmmad@gmail.com>
2026-06-14 20:20:38 +0600

    update README file

--- README.md
+++ README.md
@@ -11,3 +11,15 @@
 ## Author
 
 Ahmmad Hossain
+
+## Create Local Project
+
+mkdir git-practice-ahmmad
+cd git-practice-ahmmad
+git init
+
+mkdir src
+mkdir docs
+
+git add .
+git commit -m "Initial commit: project structure and basic program"
--- src/main.py
+++ src/main.py
@@ -4,8 +4,15 @@ from utils import add, subtract, multiply, divide
 print("Name: Ahmmad Hossain")
 print("Today's Date:", date.today())
 
+# Addition
 print("Add:", add(10, 5))
+
+# Subtraction
 print("Subtract:", subtract(10, 5))
+
+# Multiplication
 print("Multiply:", multiply(10, 5))
+
+# Division
 print("Divide:", divide(10, 2))
 print("Divide:", divide(10, 0))
